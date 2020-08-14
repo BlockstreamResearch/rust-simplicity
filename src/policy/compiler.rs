bb11d046301ebcb97e3b4724d4364f4d31a2ebf2
sanket1729 <sanket1729@gmail.com>
2020-08-13 19:57:15 -0500

    policy compiler implemented

--- src/policy/compiler.rs
+++ src/policy/compiler.rs
@@ -17,17 +17,20 @@
 //! between policy fragment and a simplicity program.
 
 use super::ast::Policy;
+use crate::bitcoin_hashes::Hash;
 use crate::bititer::BitIter;
 use crate::core::term::DagTerm;
-use crate::extension;
+use crate::core::types::pow2_types;
+use crate::extension::bitcoin::BtcNode;
+use crate::extension::jets::JetsNode::{
+    Adder32, EqV256, EqV32, LessThanV32, SchnorrAssert, Sha256,
+};
 use crate::miniscript::MiniscriptKey;
 use crate::Error;
 use crate::To32BytePubKey;
 use crate::Value;
 
 use std::rc::Rc;
-
-use crate::core::types::pow2_types;
 
 /// Scribe progra: for any value of a Simplicity type b :B, the constant function
 /// from A -> B can be realized by a Simplicity expression called scribe.  
@@ -52,51 +55,128 @@ pub fn scribe<Ext>(b: Value) -> DagTerm<(), Ext> {
     }
 }
 
-pub fn compile<Pk: MiniscriptKey + To32BytePubKey, Ext: extension::Jet>(
+/// constant function that returns false
+pub fn zero<Ext>() -> DagTerm<(), Ext> {
+    scribe(Value::sum_l(Value::Unit))
+}
+
+/// constant function that returns true
+pub fn one<Ext>() -> DagTerm<(), Ext> {
+    scribe(Value::sum_r(Value::Unit))
+}
+
+/// Cond program: The combinator to branch based on the value of a
+/// bit using case and drop. The first argument is the
+/// then clause and the second argument is the else clause
+/// [[cond st]] <0, a> = [[s]](a); [[cond st]] <1, a> = [[t]](a)
+pub fn cond<Ext>(s: Rc<DagTerm<(), Ext>>, t: Rc<DagTerm<(), Ext>>) -> DagTerm<(), Ext> {
+    DagTerm::Case(Rc::new(DagTerm::Drop(s)), Rc::new(DagTerm::Drop(t)))
+}
+
+/// Convert a single bit into u2 by pre-padding zeros
+fn u1_to_u2<Ext>(s: Rc<DagTerm<(), Ext>>) -> DagTerm<(), Ext> {
+    DagTerm::Pair(Rc::new(scribe(Value::u1(0))), s)
+}
+
+/// Convert a single bit into u4 by pre-padding zeros
+fn u1_to_u4<Ext>(s: Rc<DagTerm<(), Ext>>) -> DagTerm<(), Ext> {
+    DagTerm::Pair(Rc::new(scribe(Value::u2(0))), Rc::new(u1_to_u2(s)))
+}
+
+/// Convert a single bit into u8 by pre-padding zeros
+fn u1_to_u8<Ext>(s: Rc<DagTerm<(), Ext>>) -> DagTerm<(), Ext> {
+    DagTerm::Pair(Rc::new(scribe(Value::u4(0))), Rc::new(u1_to_u4(s)))
+}
+
+/// Convert a single bit into u16 by pre-padding zeros
+fn u1_to_u16<Ext>(s: Rc<DagTerm<(), Ext>>) -> DagTerm<(), Ext> {
+    DagTerm::Pair(Rc::new(scribe(Value::u8(0))), Rc::new(u1_to_u8(s)))
+}
+
+/// Convert a single bit into u32 by pre-padding zeros
+fn u1_to_u32<Ext>(s: Rc<DagTerm<(), Ext>>) -> DagTerm<(), Ext> {
+    DagTerm::Pair(Rc::new(scribe(Value::u16(0))), Rc::new(u1_to_u16(s)))
+}
+
+/// Compile the desired policy into a bitcoin simplicity program
+pub fn compile<Pk: MiniscriptKey + To32BytePubKey>(
     pol: &Policy<Pk>,
-) -> Result<DagTerm<(), Ext>, Error> {
-    let pk_ty = pow2_types()[9].clone();
+) -> Result<DagTerm<(), BtcNode>, Error> {
+    let two_pow_256 = pow2_types()[9].clone();
     let frag = match pol {
-        Policy::Unsatisfiable => unimplemented!(),
-        Policy::Trivial => unimplemented!(),
+        Policy::Unsatisfiable => unimplemented!(), //lookup  fail
+        Policy::Trivial => DagTerm::Unit,
         Policy::Key(ref pk) => {
             let pk_value = Value::from_bits_and_type(
                 &mut BitIter::from(pk.to_32_byte_pubkey().to_vec().into_iter()),
-                &pk_ty,
+                &two_pow_256,
             )?;
             let scribe_pk = scribe(pk_value);
             let pk_sig_pair = DagTerm::Pair(Rc::new(scribe_pk), Rc::new(DagTerm::Witness(())));
+            DagTerm::Comp(Rc::new(pk_sig_pair), Rc::new(DagTerm::Jet(SchnorrAssert)))
+        }
+        Policy::Sha256(ref h) => {
+            let hash_value = Value::from_bits_and_type(
+                &mut BitIter::from(h.into_inner().to_vec().into_iter()),
+                &two_pow_256,
+            )?;
+            // scribe target hash
+            let scribe_hash = scribe(hash_value);
+            // compute the preimage hash. An implicit contraint on the len=32 is enfored
+            // by the typesystem.
+            let computed_hash =
+                DagTerm::Comp(Rc::new(DagTerm::Witness(())), Rc::new(DagTerm::Jet(Sha256)));
+            // Check eq256 here
+            let pair = DagTerm::Pair(Rc::new(scribe_hash), Rc::new(computed_hash));
+            DagTerm::Comp(Rc::new(pair), Rc::new(DagTerm::Jet(EqV256)))
+        }
+        Policy::After(n) => {
+            let cltv = DagTerm::Ext(BtcNode::LockTime);
+            let n_value = Value::u32(*n);
+            let scribe_n = scribe(n_value);
+            let pair = DagTerm::Pair(Rc::new(scribe_n), Rc::new(cltv));
+            DagTerm::Comp(Rc::new(pair), Rc::new(DagTerm::Jet(LessThanV32)))
+        }
+        Policy::Older(n) => {
+            let csv = DagTerm::Ext(BtcNode::CurrentSequence);
+            let n_value = Value::u32(*n);
+            let scribe_n = scribe(n_value);
+            let pair = DagTerm::Pair(Rc::new(scribe_n), Rc::new(csv));
+            DagTerm::Comp(Rc::new(pair), Rc::new(DagTerm::Jet(LessThanV32)))
+        }
+        Policy::Threshold(k, ref subs) => {
+            assert!(subs.len() >= 2, "Threshold must have numbre of subs >=2");
+            let child = compile(&subs[0])?;
+            // selector denotes a bit that specifies whether the first child should be executed.
+            let selector = Rc::new(DagTerm::Witness(()));
+            // The case condition that for the current child
+            let case_term = cond(Rc::new(child), Rc::new(DagTerm::Unit));
+            let mut acc = DagTerm::Comp(Rc::clone(&selector), Rc::new(case_term));
+            let mut sum = u1_to_u32(selector);
+            for sub in &subs[1..] {
+                let child = compile(sub)?;
+                let selector = Rc::new(DagTerm::Witness(()));
+                let case_term = cond(Rc::new(child), Rc::new(DagTerm::Unit));
+
+                let curr_term = DagTerm::Comp(Rc::clone(&selector), Rc::new(case_term));
+                let selector_u32 = u1_to_u32(selector);
+
+                acc = DagTerm::Comp(Rc::new(acc), Rc::new(curr_term));
+                let full_sum = DagTerm::Comp(
+                    Rc::new(DagTerm::Pair(Rc::new(sum), Rc::new(selector_u32))),
+                    Rc::new(DagTerm::Jet(Adder32)),
+                );
+                // Discard the overflow bit.
+                // NOTE: This *assumes* that the threshold would be have 2**32 branches.
+                // FIXME: enforce this in policy specification.
+                sum = DagTerm::Drop(Rc::new(full_sum));
+            }
+            let scribe_k = scribe(Value::u32(*k as u32));
             DagTerm::Comp(
-                Rc::new(pk_sig_pair),
-                Rc::new(DagTerm::Jet(extension::jets::JetsNode::SchnorrAssert)),
+                Rc::new(DagTerm::Pair(Rc::new(scribe_k), Rc::new(sum))),
+                Rc::new(DagTerm::Jet(EqV32)),
             )
         }
-        Policy::Sha256(ref _h) => {
-            unimplemented!()
-            // use bitcoin_hashes::Hash;
-            // use bitcoin_hashes::HashEngine;
-            // let x = sha256::HashEngine::default();
-            // let midstate = x.midstate().0;
-            // let midstate_value =
-            //     Value::from_bits_and_type(&mut BitIter::from(h.to_vec().into_iter()), &pk_ty)?;
-            // let mut nodes = scribe(midstate_value);
-            // nodes.push(Node::Witness(()));
-            // nodes.push(Node::Pair(2, 1));
-            // nodes.push(Node::Jet(extension::jets::Node::Sha256HashBlock));
-            // nodes.push(Node::Comp(2, 1));
-            // // Add the equality test here
-            // let hash_value = Value::from_bits_and_type(
-            //     &mut BitIter::from(h.into_inner().to_vec().into_iter()),
-            //     &pk_ty,
-            // )?;
-            // let nodes2 = scribe(hash_value);
-            // let li = nodes2.len() + 1;
-            // // Check eq256 here
-            // nodes
-        }
-        Policy::After(_n) => unimplemented!(),
-        Policy::Older(_n) => unimplemented!(),
-        Policy::Threshold(_k, ref _subs) => unimplemented!(),
         Policy::And(ref subs) => {
             assert!(subs.len() == 2);
             let l = compile(&subs[0])?;
@@ -107,7 +187,7 @@ pub fn compile<Pk: MiniscriptKey + To32BytePubKey, Ext: extension::Jet>(
             assert!(subs.len() == 2);
             let l = compile(&subs[0])?;
             let r = compile(&subs[1])?;
-            let case_term = DagTerm::Case(Rc::new(l), Rc::new(r));
+            let case_term = cond(Rc::new(l), Rc::new(r));
             DagTerm::Comp(Rc::new(DagTerm::Witness(())), Rc::new(case_term))
         }
     };
