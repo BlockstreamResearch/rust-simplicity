6ea5525c82023fa81bee6f02dbb74364aa2a6481
sanket1729 <sanket1729@gmail.com>
2020-09-04 21:02:47 -0500

    Added macro for 1.31 support

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,15 @@
+macro_rules! matches {
+    ($expression:expr, $($pattern:tt)+) => {
+        match $expression {
+            $($pattern)+ => true,
+            ref _e => false,
+        }
+    }
+}
+
 // Code from rust 1.45.0 for assert_matches
 // Handy for testing Results or other enums
-#![cfg(test)]
+#[cfg(test)]
 macro_rules! assert_matches {
     ($expression:expr, $($pattern:tt)+) => {
         match $expression {
