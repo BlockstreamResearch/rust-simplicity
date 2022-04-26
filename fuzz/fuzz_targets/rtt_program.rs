5583824808f75b26d5990041a6b523619e1e9de4
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2022-04-26 22:32:22 +0200

    Update fuzz tests

--- fuzz/fuzz_targets/rtt_program.rs
+++ fuzz/fuzz_targets/rtt_program.rs
@@ -15,36 +15,37 @@
 extern crate simplicity;
 
 use simplicity::bititer::BitIter;
+use simplicity::encode::BitWriter;
 use simplicity::extension::dummy::DummyNode;
-use simplicity::encode::{self, BitWrite};
+use simplicity::{decode, encode};
 
 fn do_test(data: &[u8]) {
-    let mut read_iter = BitIter::new(data.iter().cloned());
-    if let Ok(prog) = encode::decode_program_no_witness::<_, DummyNode>(&mut read_iter) {
-        let mut w = encode::BitWriter::new(Vec::<u8>::new());
-        let mut write_len = encode::encode_natural(prog.len(), &mut w)
-            .expect("encoding natural");
-        for node in prog.iter() {
-            write_len += encode::encode_node_no_witness(node, &mut w)
-                .expect("encoding node");
-        }
+    let mut iter = BitIter::new(data.iter().cloned());
+
+    if let Ok(program) = decode::decode_program_no_witness::<_, DummyNode>(&mut iter) {
+        let bit_len = iter.n_total_read();
+
+        let mut sink = Vec::<u8>::new();
+        let mut w = BitWriter::from(&mut sink);
+        encode::encode_program_no_witness(&program, &mut w).expect("encoding to vector");
         w.flush_all().expect("flushing");
+        // println!("{:?}", program);
+        assert_eq!(w.n_total_written(), bit_len);
 
-        assert_eq!(w.n_written(), write_len);
-        assert_eq!(read_iter.n_total_read(), write_len);
-
-        let mut output = w.into_inner();
-        if write_len % 8 != 0 {
-            let mask = !(1u8 << (8 - (write_len % 8)));
-            let idx = output.len() - 1;
-            output[idx] |= data[idx] & mask;
+        // decode_program_no_witness() may stop reading `data` mid-byte:
+        // copy trailing bits from `data` to `sink`
+        if bit_len % 8 != 0 {
+            let mask = !(1u8 << (8 - (bit_len % 8)));
+            let idx = sink.len() - 1;
+            sink[idx] |= data[idx] & mask;
         }
-        assert_eq!(output, &data[0..output.len()]);
+        assert_eq!(sink, &data[0..sink.len()]);
     }
 }
 
 #[cfg(feature = "afl")]
-#[macro_use] extern crate afl;
+#[macro_use]
+extern crate afl;
 #[cfg(feature = "afl")]
 fn main() {
     fuzz!(|data| {
@@ -53,7 +54,8 @@ fn main() {
 }
 
 #[cfg(feature = "honggfuzz")]
-#[macro_use] extern crate honggfuzz;
+#[macro_use]
+extern crate honggfuzz;
 #[cfg(feature = "honggfuzz")]
 fn main() {
     loop {
