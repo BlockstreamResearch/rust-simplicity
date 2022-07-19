0cf774db6b5eddc388a4e7e39446584a3e797572
Christian Lewe <ch_lewe@agdsn.me>
2022-07-19 21:25:52 +0200

    Update policy compiler
    - add unsatisfiable policies
    - fix public key, or, threshold

--- src/policy/compiler.rs
+++ src/policy/compiler.rs
@@ -21,118 +21,124 @@ use crate::core::TermDag;
 use crate::core::Value;
 use crate::jet;
 use crate::jet::application::Bitcoin;
-use crate::jet::Application;
+use crate::merkle::cmr::Cmr;
 use crate::miniscript::MiniscriptKey;
 use crate::policy::key::PublicKey32;
 use crate::Error;
 use std::rc::Rc;
 
-/// Convert a single bit into u2 by pre-padding zeros
-fn u1_to_u2<App: Application>(s: Rc<TermDag<(), App>>) -> Rc<TermDag<(), App>> {
-    TermDag::pair(TermDag::scribe(&Value::u1(0)), s)
-}
-
-/// Convert a single bit into u4 by pre-padding zeros
-fn u1_to_u4<App: Application>(s: Rc<TermDag<(), App>>) -> Rc<TermDag<(), App>> {
-    TermDag::pair(TermDag::scribe(&Value::u2(0)), u1_to_u2(s))
-}
-
-/// Convert a single bit into u8 by pre-padding zeros
-fn u1_to_u8<App: Application>(s: Rc<TermDag<(), App>>) -> Rc<TermDag<(), App>> {
-    TermDag::pair(TermDag::scribe(&Value::u4(0)), u1_to_u4(s))
-}
-
-/// Convert a single bit into u16 by pre-padding zeros
-fn u1_to_u16<App: Application>(s: Rc<TermDag<(), App>>) -> Rc<TermDag<(), App>> {
-    TermDag::pair(TermDag::scribe(&Value::u8(0)), u1_to_u8(s))
-}
-
-/// Convert a single bit into u32 by pre-padding zeros
-fn u1_to_u32<App: Application>(s: Rc<TermDag<(), App>>) -> Rc<TermDag<(), App>> {
-    TermDag::pair(TermDag::scribe(&Value::u16(0)), u1_to_u16(s))
-}
-
-/// Compile the desired policy into a bitcoin simplicity program
+/// Compile the given policy into a Simplicity DAG.
 pub fn compile<Pk: MiniscriptKey + PublicKey32>(
-    pol: &Policy<Pk>,
+    policy: &Policy<Pk>,
 ) -> Result<Rc<TermDag<(), Bitcoin>>, Error> {
-    let frag = match pol {
-        Policy::Unsatisfiable => unimplemented!(), //lookup  fail
+    let dag = match policy {
+        // TODO: Choose specific Merkle roots for unsatisfiable policies
+        Policy::Unsatisfiable => TermDag::fail(Cmr::from([0; 32]), Cmr::from([0; 32])),
         Policy::Trivial => TermDag::unit(),
-        Policy::Key(ref pk) => {
-            let pk_value = Value::u256_from_slice(&pk.to_32_bytes());
-            let scribe_pk = TermDag::scribe(&pk_value);
-            let pk_sig_pair = TermDag::pair(scribe_pk, TermDag::witness(()));
-            TermDag::comp(pk_sig_pair, TermDag::jet(&jet::bitcoin::BIP_0340_VERIFY))
-        }
-        Policy::Sha256(ref h) => {
-            let hash_value = Value::u256_from_slice(h);
-            let scribe_hash = TermDag::scribe(&hash_value);
-            let computed_hash =
-                TermDag::comp(TermDag::witness(()), TermDag::jet(&jet::bitcoin::SHA256));
-            let pair = TermDag::pair(scribe_hash, computed_hash);
-            TermDag::comp(pair, TermDag::jet(&jet::bitcoin::EQ256_VERIFY))
+        Policy::Key(key) => {
+            let key_value = Value::u256_from_slice(&key.to_32_bytes());
+            let pair_key_msg = TermDag::pair(
+                TermDag::scribe(&key_value),
+                TermDag::jet(&jet::bitcoin::SIGHASH_ALL),
+            );
+            let pair_key_msg_sig = TermDag::pair(pair_key_msg, TermDag::witness(()));
+            TermDag::comp(
+                pair_key_msg_sig,
+                TermDag::jet(&jet::bitcoin::BIP_0340_VERIFY),
+            )
         }
         Policy::After(n) => {
             let n_value = Value::u32(*n);
-            let scribe_n = TermDag::scribe(&n_value);
-            let cltv = TermDag::jet(&jet::bitcoin::LOCK_TIME);
-            let pair = TermDag::pair(scribe_n, cltv);
-            TermDag::comp(pair, TermDag::jet(&jet::bitcoin::LT32_VERIFY))
+            let pair_n_locktime = TermDag::pair(
+                TermDag::scribe(&n_value),
+                TermDag::jet(&jet::bitcoin::LOCK_TIME),
+            );
+            TermDag::comp(pair_n_locktime, TermDag::jet(&jet::bitcoin::LT32_VERIFY))
         }
         Policy::Older(n) => {
             let n_value = Value::u32(*n);
-            let scribe_n = TermDag::scribe(&n_value);
-            let csv = TermDag::jet(&jet::bitcoin::CURRENT_SEQUENCE);
-            let pair = TermDag::pair(scribe_n, csv);
-            TermDag::comp(pair, TermDag::jet(&jet::bitcoin::LT32_VERIFY))
+            let pair_n_sequence = TermDag::pair(
+                TermDag::scribe(&n_value),
+                TermDag::jet(&jet::bitcoin::CURRENT_SEQUENCE),
+            );
+            TermDag::comp(pair_n_sequence, TermDag::jet(&jet::bitcoin::LT32_VERIFY))
         }
-        Policy::Threshold(k, ref subs) => {
-            assert!(subs.len() >= 2, "Threshold must have numbre of subs >=2");
-            let child = compile(&subs[0])?;
-            // selector denotes a bit that specifies whether the first child should be executed.
-            let selector = TermDag::witness(());
-            // The case condition that for the current child
-            let case_term = TermDag::cond(child, TermDag::unit());
-            let mut acc = TermDag::comp(selector.clone(), case_term);
-            let mut sum = u1_to_u32(selector);
-            for sub in &subs[1..] {
+        Policy::Sha256(hash) => {
+            let hash_value = Value::u256_from_slice(hash);
+            let computed_hash =
+                TermDag::comp(TermDag::witness(()), TermDag::jet(&jet::bitcoin::SHA256));
+            let pair_hash_computed_hash =
+                TermDag::pair(TermDag::scribe(&hash_value), computed_hash);
+            TermDag::comp(
+                pair_hash_computed_hash,
+                TermDag::jet(&jet::bitcoin::EQ256_VERIFY),
+            )
+        }
+        Policy::And(sub_policies) => {
+            assert_eq!(2, sub_policies.len());
+
+            let left = compile(&sub_policies[0])?;
+            let right = compile(&sub_policies[1])?;
+            TermDag::comp(left, right)
+        }
+        Policy::Or(sub_policies) => {
+            assert_eq!(2, sub_policies.len());
+
+            let left = compile(&sub_policies[0])?;
+            let right = compile(&sub_policies[1])?;
+            // Cannot use TermDag::cond lest the witness is in reverse order
+            let cond_left_right = TermDag::case(TermDag::drop(left), TermDag::drop(right));
+            let selector = TermDag::pair(TermDag::witness(()), TermDag::unit());
+            TermDag::comp(selector, cond_left_right)
+        }
+        Policy::Threshold(k, sub_policies) => {
+            assert!(
+                sub_policies.len() >= 2,
+                "Thresholds must have at least two sub-policies"
+            );
+
+            // 1 -> 1
+            let child = compile(&sub_policies[0])?;
+            // 1 -> 2 x 1
+            let selector = TermDag::pair(TermDag::witness(()), TermDag::unit());
+            // 1 -> 2^32
+            let child_one = TermDag::comp(child, TermDag::scribe(&Value::u32(1)));
+            // 2 x 1 -> 2^32
+            let child_one_or_zero = TermDag::cond(child_one, TermDag::scribe(&Value::u32(0)));
+            // 1 -> 2^32
+            let mut sum = TermDag::comp(selector, child_one_or_zero);
+
+            for sub in &sub_policies[1..] {
+                // 1 -> 1
                 let child = compile(sub)?;
-                let selector = TermDag::witness(());
-                let case_term = TermDag::cond(child, TermDag::unit());
-
-                let curr_term = TermDag::comp(selector.clone(), case_term);
-                let selector_u32 = u1_to_u32(selector);
-
-                acc = TermDag::comp(acc, curr_term);
+                // 1 -> 2 x 1
+                let selector = TermDag::pair(TermDag::witness(()), TermDag::unit());
+                // 1 -> 2^32
+                let child_one = TermDag::comp(child, TermDag::scribe(&Value::u32(1)));
+                // 2 x 1 -> 2^32
+                let child_one_or_zero = TermDag::cond(child_one, TermDag::scribe(&Value::u32(0)));
+                // 1 -> 2^32
+                let local_summand = TermDag::comp(selector, child_one_or_zero);
+                // 1 -> 2 x 2^32
                 let full_sum = TermDag::comp(
-                    TermDag::pair(sum, selector_u32),
+                    TermDag::pair(sum, local_summand),
                     TermDag::jet(&jet::bitcoin::ADD32),
                 );
                 // Discard the overflow bit.
-                // NOTE: This *assumes* that the threshold would be have 2**32 branches.
-                // FIXME: enforce this in policy specification.
-                sum = TermDag::drop(full_sum);
+                // FIXME: enforce that sum of weights is less than 2^32
+                // 1 -> 2^32
+                sum = TermDag::comp(full_sum, TermDag::drop(TermDag::iden()));
             }
+
+            // 1 -> 2^32
             let scribe_k = TermDag::scribe(&Value::u32(*k as u32));
+            // 1 -> 1
             TermDag::comp(
                 TermDag::pair(scribe_k, sum),
                 TermDag::jet(&jet::bitcoin::EQ32_VERIFY),
             )
         }
-        Policy::And(ref subs) => {
-            assert!(subs.len() == 2);
-            let l = compile(&subs[0])?;
-            let r = compile(&subs[1])?;
-            TermDag::comp(l, r)
-        }
-        Policy::Or(ref subs) => {
-            assert!(subs.len() == 2);
-            let l = compile(&subs[0])?;
-            let r = compile(&subs[1])?;
-            let case_term = TermDag::cond(l, r);
-            TermDag::comp(TermDag::witness(()), case_term)
-        }
     };
-    Ok(frag)
+
+    Ok(dag)
 }
