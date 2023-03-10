5dd26c54b0090e957a3d431485c7837494a8b382
Christian Lewe <ch_lewe@agdsn.me>
2023-03-10 21:04:53 +0100

    Dummy ElementsEnv

--- src/jet/elements/environment.rs
+++ src/jet/elements/environment.rs
@@ -116,6 +116,33 @@ impl ElementsEnv {
     }
 }
 
+#[cfg(test)]
+impl ElementsEnv {
+    /// Return a dummy Elements environment
+    pub fn dummy() -> Self {
+        let ctrl_blk: [u8; 33] = [
+            0xc0, 0xeb, 0x04, 0xb6, 0x8e, 0x9a, 0x26, 0xd1, 0x16, 0x04, 0x6c, 0x76, 0xe8, 0xff,
+            0x47, 0x33, 0x2f, 0xb7, 0x1d, 0xda, 0x90, 0xff, 0x4b, 0xef, 0x53, 0x70, 0xf2, 0x52,
+            0x26, 0xd3, 0xbc, 0x09, 0xfc,
+        ];
+
+        ElementsEnv::new(
+            Arc::new(elements::Transaction {
+                version: u32::default(),
+                lock_time: elements::PackedLockTime::ZERO,
+                input: Vec::default(),
+                output: Vec::default(),
+            }),
+            Vec::default(),
+            u32::default(),
+            Cmr::from([0; 32]),
+            ControlBlock::from_slice(&ctrl_blk).unwrap(),
+            None,
+            elements::BlockHash::all_zeros(),
+        )
+    }
+}
+
 /// Helper trait for writing various components of
 /// Simplicity transactions (assets, values) into bit machine.
 pub(super) trait SimplicityEncodable {
