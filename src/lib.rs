6102c4761e4eae3c4a2cc312895ec477695b33a1
Christian Lewe <ch_lewe@agdsn.me>
2023-04-28 11:29:34 -0500

    Change extern crate to pub use

--- src/lib.rs
+++ src/lib.rs
@@ -14,15 +14,15 @@
 
 #![allow(clippy::redundant_field_names, clippy::identity_op)]
 
-extern crate bitcoin_hashes;
-extern crate byteorder;
-
 #[cfg(feature = "serde")]
 pub use actual_serde as serde;
 #[cfg(feature = "bitcoin")]
 pub use bitcoin;
 #[cfg(feature = "elements")]
 pub use elements;
+
+pub use bitcoin_hashes;
+pub use byteorder;
 pub use elements_miniscript as miniscript;
 
 #[macro_use]
