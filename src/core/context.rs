06a450714bfea8a2ea49ecde7a863707f2310e76
Andrew Poelstra <apoelstra@wpsoftware.net>
2023-05-15 21:43:16 +0000

    context: add powers of two to the Context object
    We will need them to decode `Word` jets (and there we need 32 of them,
    rather than 9), and it is also useful during type inference.

--- src/core/context.rs
+++ src/core/context.rs
@@ -1,21 +1,52 @@
-use crate::core::types::VariableFactory;
-use crate::jet::Jet;
+
 use std::marker::PhantomData;
+use std::sync::Arc;
+
+use crate::core::types::{precomputed_square, RcVar, Type, Variable, VariableFactory};
+use crate::jet::Jet;
 
 /// Context for constructing a Simplicity program
 #[allow(dead_code)]
 pub struct Context<J: Jet> {
     pub(crate) naming: VariableFactory,
+    pow2: Vec<RcVar>,
     _jet: PhantomData<J>,
 }
 
 impl<J: Jet> Context<J> {
     /// Create a new context.
     pub fn new() -> Self {
+        let two = Variable::precomputed_2();
+        let pow2 = std::iter::successors(
+            Some(two),
+            |prev| Some(precomputed_square(prev)),
+        ).take(32).collect();
+
         Self {
             naming: VariableFactory::new(),
+            pow2,
             _jet: PhantomData,
         }
+    }
+
+    /// Accessor for the nth power of two as a type
+    pub fn nth_power_of_2_type(&self, n: usize) -> Arc<Type> {
+        assert!(
+            n < self.pow2.len(),
+            "we have cached only {} powers of 2 but {} was requested",
+            self.pow2.len(), n,
+        );
+        self.pow2[n].borrow().precomputed_finalized()
+    }
+
+    /// Accessor for the nth power of two as an `RcVar`
+    pub (crate) fn nth_power_of_2_rcvar(&self, n: usize) -> &RcVar {
+        assert!(
+            n < self.pow2.len(),
+            "we have cached only {} powers of 2 but {} was requested",
+            self.pow2.len(), n,
+        );
+        &self.pow2[n]
     }
 }
 
