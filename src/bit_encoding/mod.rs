c082ddb56d9baadab8b040b85e94e4b0e0196c1a
Andrew Poelstra <apoelstra@wpsoftware.net>
2023-06-10 17:41:06 +0000

    encode/decode: refactor into new module bit_encoding
    This is pretty-much a move-only commit.

---
+++ src/bit_encoding/mod.rs
@@ -0,0 +1,28 @@
+// Rust Simplicity Library
+// Written in 2023 by
+//   Andrew Poelstra <rust-simplicity@wpsoftware.net>
+//   Christian Lewe <clewe@blockstream.com>
+//   Sanket Kanjalkar <sanket1729@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication
+// along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//
+
+//! The Simplicity Bit Encoding
+//!
+//! This module provides functionality to encode and decode Simplicity programs
+//! and expresisons at rest and on the blockchain. For [`core::redeem::RedeemNode`]s,
+//! this encoding matches the consensus encoding defined by the Simplicity Tech
+//! Report.
+//!
+
+pub mod bititer;
+pub mod bitwriter;
+pub mod decode;
+pub mod encode;
--- src/lib.rs
+++ src/lib.rs
@@ -34,13 +34,10 @@ pub use elements_miniscript as miniscript;
 mod macros;
 
 mod analysis;
+pub mod bit_encoding;
 pub mod bit_machine;
-pub mod bititer;
-pub mod bitwriter;
 pub mod core;
 pub mod dag;
-mod decode;
-mod encode;
 pub mod jet;
 pub mod merkle;
 #[cfg(feature = "elements")]
@@ -49,6 +46,11 @@ pub mod types;
 // #[cfg(test)]
 // mod test_progs;
 mod util;
+
+pub use bit_encoding::bititer;
+pub use bit_encoding::bitwriter;
+use bit_encoding::decode;
+use bit_encoding::encode;
 
 #[cfg(feature = "elements")]
 pub use crate::policy::Policy;
