73e071ff47dce4798bd0be28326ee16289080d1e
Andrew Poelstra <apoelstra@wpsoftware.net>
2023-07-08 22:18:29 +0000

    human_encoding: add serialization support

--- src/human_encoding/mod.rs
+++ src/human_encoding/mod.rs
@@ -21,9 +21,11 @@
 //!
 
 mod named_node;
+mod serialize;
 
+use crate::dag::{DagLike, MaxSharing};
 use crate::jet::Jet;
-use crate::node::CommitNode;
+use crate::node::{self, CommitNode};
 
 use std::collections::HashMap;
 use std::str;
@@ -43,5 +45,99 @@ impl<J: Jet> Forest<J> {
         let mut roots = HashMap::new();
         roots.insert("main".into(), root);
         Forest { roots }
+    }
+
+    /// Serialize the program in human-readable form
+    pub fn string_serialize(&self) -> String {
+        struct Print {
+            expr_str: String,  // The X = Y part
+            arrow_str: String, // The :: A -> B part
+            cmr_str: String,   // The -- <cmr> part
+        }
+        fn print_lines(lines: &[Print], skip_before_end: bool) -> String {
+            let mut ret = String::new();
+            let expr_width = lines.iter().map(|line| line.expr_str.len()).max().unwrap();
+            let arrow_width = lines.iter().map(|line| line.arrow_str.len()).max().unwrap();
+            let last_line = lines.len();
+            for (n, line) in lines.iter().enumerate() {
+                if skip_before_end && n == last_line - 1 {
+                    ret += "\n";
+                }
+                ret += &format!(
+                    "{0:1$} {2:3$} {4}\n",
+                    line.expr_str, expr_width, line.arrow_str, arrow_width, line.cmr_str
+                );
+            }
+            ret
+        }
+
+        let mut witness_lines = vec![];
+        let mut const_lines = vec![];
+        let mut program_lines = vec![];
+        // Pass 1: compute string data for every node
+        for root in self.roots.values() {
+            for data in root.as_ref().post_order_iter::<MaxSharing<_>>() {
+                let node = data.node;
+                let name = node.name();
+                let mut expr_str = match node.inner() {
+                    node::Inner::Fail(entropy) => format!("{} := fail {}", name, entropy),
+                    node::Inner::Jet(ref j) => format!("{} := jet_{}", name, j),
+                    node::Inner::Word(ref v) => {
+                        format!("{} := const {}", name, serialize::DisplayWord(v))
+                    }
+                    inner => format!("{} := {}", name, inner),
+                };
+                if let Some(child) = node.left_child() {
+                    expr_str.push(' ');
+                    expr_str.push_str(child.name());
+                }
+                if let Some(child) = node.right_child() {
+                    expr_str.push(' ');
+                    expr_str.push_str(child.name());
+                }
+
+                let arrow = node.arrow();
+                let arrow_str = format!(": {} -> {}", arrow.source, arrow.target).replace('×', "*"); // for human-readable encoding stick with ASCII
+
+                // All witnesses have the same CMR so don't bother printing it
+                let cmr_str = if let node::Inner::Witness(..) = node.inner() {
+                    String::new()
+                } else {
+                    format!("-- cmr {:.8}...", node.cmr())
+                };
+
+                let print = Print {
+                    expr_str,
+                    arrow_str,
+                    cmr_str,
+                };
+                if let node::Inner::Witness(..) = node.inner() {
+                    witness_lines.push(print);
+                } else if let node::Inner::Word(..) = node.inner() {
+                    const_lines.push(print);
+                } else {
+                    program_lines.push(print);
+                }
+            }
+        }
+
+        // Pass 2: actually print everything
+        let mut ret = String::new();
+        if !witness_lines.is_empty() {
+            ret += "-- Witnesses\n";
+            ret += &print_lines(&witness_lines, false);
+            ret += "\n";
+        }
+        if !const_lines.is_empty() {
+            // FIXME detect scribes
+            ret += "-- Constants\n";
+            ret += &print_lines(&const_lines, false);
+            ret += "\n";
+        }
+        if !program_lines.is_empty() {
+            ret += "-- Program code\n";
+            ret += &print_lines(&program_lines, true /* add a blank line before main */);
+        }
+        ret
     }
 }
