efdea5f5d53440790f25279c984d492eba7bae5a
Christian Lewe <ch_lewe@agdsn.me>
2023-08-21 19:51:23 +0200

    Value: Update docs

--- src/value.rs
+++ src/value.rs
@@ -25,15 +25,27 @@ use std::fmt;
 use std::hash::Hash;
 use std::sync::Arc;
 
-/// Simplicity value.
+/// Value of some type.
 ///
-/// _Unit_ is the base value and contains no information.
+/// The _unit value_ is the only value of the _unit type_.
+/// This is the basis for everything we are doing.
+/// Because there is only a single unit value, there is no information contained in it.
+/// Instead, we wrap unit values in sum and product values to encode information.
 ///
-/// The zero bit is represented as the _left sum_ of unit,
-/// and the one bit is represented as the _right sum_ of unit.
+/// A _sum value_ wraps another value.
+/// The _left sum value_ `L(a)` wraps a value `a` from the _left type_ `A`.
+/// The _right sum value_ `R(b)` wraps a value `b` from the _right type_ `B`.
+/// The type of the sum value is the _sum type_ `A + B` of the left type and the right type.
 ///
-/// Bitstrings are represented as a tree of _products_ (a.k.a. a tuple)
-/// with sums of unit as its leaves (a.k.a. bits).
+/// We represent the false bit as a left value that wraps a unit value.
+/// The true bit is represented as a right value that wraps a unit value.
+///
+/// A _product value_ `(a, b)` wraps two values:
+/// a value `a` from the _left type_ `A` and a value `b` from the _right type_ `B`.
+/// The type of the product value is the _product type_ `A × B` of the left type and the right type.
+///
+/// We represent bit strings (tuples of bits) as trees of nested product values
+/// that have bit values (sum values wrapping the unit value) at their leaves.
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Value {
     /// Unit value
@@ -137,20 +149,23 @@ impl Value {
         Value::prod(Value::u16(w0), Value::u16(w1))
     }
 
-    /// Encode a 32-bit number as a value
+    /// Encode a 64-bit number as a value
     pub fn u64(n: u64) -> Arc<Self> {
         let w0 = (n >> 32) as u32;
         let w1 = (n & 0xffff_ffff) as u32;
         Value::prod(Value::u32(w0), Value::u32(w1))
     }
 
+    /// Encode a 128-bit number as a value
     pub fn u128(n: u128) -> Arc<Self> {
         let w0 = (n >> 64) as u64;
         let w1 = n as u64; // Cast safety: picking last 64 bits
         Value::prod(Value::u64(w0), Value::u64(w1))
     }
 
-    /// Encode a 32 byte number into value. Useful for encoding 32 pubkeys/hashes
+    /// Encode a 32-byte number as a value
+    ///
+    /// Useful for encoding public keys and hashes
     pub fn u256_from_slice(v: &[u8]) -> Arc<Self> {
         assert_eq!(32, v.len(), "Expect 32-byte slice");
 
@@ -166,8 +181,9 @@ impl Value {
         )
     }
 
-    /// Encode a 64(pair(32, 32)) byte number into value.
-    /// Useful for encoding 64 byte signatures
+    /// Encode a 64-byte number as a value
+    ///
+    /// Useful for encoding signatures
     pub fn u512_from_slice(v: &[u8]) -> Arc<Self> {
         assert_eq!(64, v.len(), "Expect 64-byte slice");
 
