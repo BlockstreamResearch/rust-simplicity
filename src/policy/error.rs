9bcef5155709e34b4b22379825cc10e280139d6d
Christian Lewe <ch_lewe@agdsn.me>
2024-01-29 15:25:13 +0100

    Lib: Fix license headers
    My header conversion script missed some files

--- src/policy/error.rs
+++ src/policy/error.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: CC0-1.0
+
 use std::fmt;
 
 pub enum Error {
--- src/policy/mod.rs
+++ src/policy/mod.rs
@@ -1,16 +1,4 @@
-// Simplcity Policy language
-// Written in 2020 by
-//     Sanket Kanjalkar <sanket1729@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all
-// copyright and related and neighboring rights to this software to
-// the public domain worldwide. This software is distributed without
-// any warranty.
-//
-// You should have received a copy of the CC0 Public Domain Dedication
-// along with this software.
-// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//
+// SPDX-License-Identifier: CC0-1.0
 
 //! # Function-like Expression Language
 //! This language is similar to policy language in miniscript, but
