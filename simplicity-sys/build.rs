3fd64d3f5c49b1c8644aaa26a9420e006c287369
Christian Lewe <ch_lewe@agdsn.me>
2024-02-09 10:59:19 +0900

    FFI: Update build script

--- simplicity-sys/build.rs
+++ simplicity-sys/build.rs
@@ -7,35 +7,15 @@ fn main() {
     // rerun if changes to the C code
     println!("cargo:rerun-if-changed=depend");
     let simplicity_path = Path::new("depend/simplicity");
-    let files: Vec<_> = vec![
-        "bitstream.c",
-        "dag.c",
-        "deserialize.c",
-        "eval.c",
+    let jet_files: Vec<_> = vec![
         "frame.c",
         "jets.c",
         "jets-secp256k1.c",
         "rsort.c",
         "sha256.c",
-        "type.c",
-        "typeInference.c",
         "primitive/elements/env.c",
         "primitive/elements/ops.c",
-        "primitive/elements/exec.c",
         "primitive/elements/jets.c",
-        "primitive/elements/primitive.c",
-    ]
-    .into_iter()
-    .map(|x| simplicity_path.join(x))
-    .collect();
-    let test_files: Vec<_> = vec![
-        // "test.c",
-        "ctx8Pruned.c",
-        "ctx8Unpruned.c",
-        "hashBlock.c",
-        "schnorr0.c",
-        "schnorr6.c",
-        "primitive/elements/checkSigHashAllTx1.c",
     ]
     .into_iter()
     .map(|x| simplicity_path.join(x))
@@ -46,12 +26,35 @@ fn main() {
         .std("c11")
         .flag_if_supported("-fno-inline-functions")
         .opt_level(2)
-        .files(files)
-        .files(test_files)
+        .files(jet_files)
         .file(Path::new("depend/wrapper.c"))
         .file(Path::new("depend/env.c"))
         .file(Path::new("depend/jets_wrapper.c"))
         .include(simplicity_path.join("include"));
+
+    if cfg!(feature = "test-utils") {
+        let test_files: Vec<_> = vec![
+            "bitstream.c",
+            "dag.c",
+            "deserialize.c",
+            "eval.c",
+            "type.c",
+            "typeInference.c",
+            "primitive/elements/exec.c",
+            "primitive/elements/primitive.c",
+            "ctx8Pruned.c",
+            "ctx8Unpruned.c",
+            "hashBlock.c",
+            "schnorr0.c",
+            "schnorr6.c",
+            "primitive/elements/checkSigHashAllTx1.c",
+        ]
+        .into_iter()
+        .map(|x| simplicity_path.join(x))
+        .collect();
+
+        build.files(test_files);
+    }
 
     if cfg!(not(fuzzing)) {
         build.define("PRODUCTION", None);
