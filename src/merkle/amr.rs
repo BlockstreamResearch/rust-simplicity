BlockstreamResearch/rust-simplicity

commit fac715d90a3ac19c7b562df3adc9c5729a85fae1

Final: split_* -> as_*
Rename the split methods and return a reference instead of cloning.

src/merkle/amr.rs
@@ -48,7 +48,7 @@ impl Amr {
     /// Produce a CMR for an injl combinator
     pub fn injl(ty: &FinalArrow, child: Amr) -> Self {
         let a = &ty.source;
-        let (b, c) = ty.target.split_sum().unwrap();
+        let (b, c) = ty.target.as_sum().unwrap();
         Self::INJL_IV
             .update(a.tmr().into(), b.tmr().into())
             .update(c.tmr().into(), child)
@@ -57,7 +57,7 @@ impl Amr {
     /// Produce a CMR for an injr combinator
     pub fn injr(ty: &FinalArrow, child: Amr) -> Self {
         let a = &ty.source;
-        let (b, c) = ty.target.split_sum().unwrap();
+        let (b, c) = ty.target.as_sum().unwrap();
         Self::INJR_IV
             .update(a.tmr().into(), b.tmr().into())
             .update(c.tmr().into(), child)
@@ -65,7 +65,7 @@ impl Amr {
 
     /// Produce a CMR for a take combinator
     pub fn take(ty: &FinalArrow, child: Amr) -> Self {
-        let (a, b) = ty.source.split_product().unwrap();
+        let (a, b) = ty.source.as_sum().unwrap();
         let c = &ty.target;
         Self::TAKE_IV
             .update(a.tmr().into(), b.tmr().into())
@@ -74,7 +74,7 @@ impl Amr {
 
     /// Produce a CMR for a drop combinator
     pub fn drop(ty: &FinalArrow, child: Amr) -> Self {
-        let (a, b) = ty.source.split_product().unwrap();
+        let (a, b) = ty.source.as_product().unwrap();
         let c = &ty.target;
         Self::DROP_IV
             .update(a.tmr().into(), b.tmr().into())
@@ -93,8 +93,8 @@ impl Amr {
     }
 
     fn case_helper(iv: Amr, ty: &FinalArrow, left: Amr, right: Amr) -> Self {
-        let (sum_a_b, c) = ty.source.split_product().unwrap();
-        let (a, b) = sum_a_b.split_sum().unwrap();
+        let (sum_a_b, c) = ty.source.as_product().unwrap();
+        let (a, b) = sum_a_b.as_sum().unwrap();
         let d = &ty.target;
         iv.update(a.tmr().into(), b.tmr().into())
             .update(c.tmr().into(), d.tmr().into())
@@ -136,7 +136,7 @@ impl Amr {
     /// Produce a CMR for a disconnect combinator
     pub fn disconnect(ty: &FinalArrow, right_arrow: &FinalArrow, left: Amr, right: Amr) -> Self {
         let a = &ty.source;
-        let (b, d) = ty.target.split_product().unwrap();
+        let (b, d) = ty.target.as_product().unwrap();
         let c = &right_arrow.source;
         Self::DISCONNECT_IV
             .update(a.tmr().into(), b.tmr().into())

src/bit_machine/mod.rs
@@ -257,14 +257,14 @@ impl BitMachine {
                     self.copy(size_a);
                 }
                 node::Inner::InjL(left) => {
-                    let (b, _c) = ip.arrow().target.split_sum().unwrap();
+                    let (b, _c) = ip.arrow().target.as_sum().unwrap();
                     let padl_b_c = ip.arrow().target.bit_width() - b.bit_width() - 1;
                     self.write_bit(false);
                     self.skip(padl_b_c);
                     call_stack.push(CallStack::Goto(left));
                 }
                 node::Inner::InjR(left) => {
-                    let (_b, c) = ip.arrow().target.split_sum().unwrap();
+                    let (_b, c) = ip.arrow().target.as_sum().unwrap();
                     let padr_b_c = ip.arrow().target.bit_width() - c.bit_width() - 1;
                     self.write_bit(true);
                     self.skip(padr_b_c);
@@ -305,7 +305,7 @@ impl BitMachine {
                 }
                 node::Inner::Take(left) => call_stack.push(CallStack::Goto(left)),
                 node::Inner::Drop(left) => {
-                    let size_a = ip.arrow().source.split_product().unwrap().0.bit_width();
+                    let size_a = ip.arrow().source.as_product().unwrap().0.bit_width();
                     self.fwd(size_a);
                     call_stack.push(CallStack::Back(size_a));
                     call_stack.push(CallStack::Goto(left));
@@ -313,8 +313,8 @@ impl BitMachine {
                 node::Inner::Case(..) | node::Inner::AssertL(..) | node::Inner::AssertR(..) => {
                     let choice_bit = self.read[self.read.len() - 1].peek_bit(&self.data);
 
-                    let (sum_a_b, _c) = ip.arrow().source.split_product().unwrap();
-                    let (a, b) = sum_a_b.split_sum().unwrap();
+                    let (sum_a_b, _c) = ip.arrow().source.as_product().unwrap();
+                    let (a, b) = sum_a_b.as_sum().unwrap();
                     let size_a = a.bit_width();
                     let size_b = b.bit_width();
 

src/types/final_data.rs
@@ -196,23 +196,23 @@ impl Final {
         &self.bound
     }
 
-    /// Returns whether this is the unit type
+    /// Check if the type is a unit.
     pub fn is_unit(&self) -> bool {
         self.bound == CompleteBound::Unit
     }
 
-    /// Return both children, if the type is a sum type
-    pub fn split_sum(&self) -> Option<(Arc<Self>, Arc<Self>)> {
+    /// Access the inner types of a sum type.
+    pub fn as_sum(&self) -> Option<(&Self, &Self)> {
         match &self.bound {
-            CompleteBound::Sum(left, right) => Some((left.clone(), right.clone())),
+            CompleteBound::Sum(left, right) => Some((left.as_ref(), right.as_ref())),
             _ => None,
         }
     }
 
-    /// Return both children, if the type is a product type
-    pub fn split_product(&self) -> Option<(Arc<Self>, Arc<Self>)> {
+    /// Access the inner types of a product type.
+    pub fn as_product(&self) -> Option<(&Self, &Self)> {
         match &self.bound {
-            CompleteBound::Product(left, right) => Some((left.clone(), right.clone())),
+            CompleteBound::Product(left, right) => Some((left.as_ref(), right.as_ref())),
             _ => None,
         }
     }

commit 88ac7b9add53b9b68dfd4f174d158c8a364a3e11

Fix AMR computation
The error was introduced in fac715d90a3ac19c7b562df3adc9c5729a85fae1

The error makes the AMR computation panic.

src/merkle/amr.rs
@@ -65,7 +65,7 @@ impl Amr {
 
     /// Produce a CMR for a take combinator
     pub fn take(ty: &FinalArrow, child: Amr) -> Self {
-        let (a, b) = ty.source.as_sum().unwrap();
+        let (a, b) = ty.source.as_product().unwrap();
         let c = &ty.target;
         Self::TAKE_IV
             .update(a.tmr().into(), b.tmr().into())