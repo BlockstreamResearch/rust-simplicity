cbc22ffe964f876dc40e3eba4354adbad11550f3
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-07-04 18:36:00 +0000

    fixups to vendor-simplicity.sh
    The existing vendor-simplicity.sh has a few issues -- notably, it
    doesn't thoroughly check its input or its environment, it switches
    between directories in an unreliable way, and (most importantly) it does
    not patch simplicity_alloc.h, meaning that if you just run the script,
    you will get memory issues with the resulting libsimplicity when running
    the Rust unit tests.

--- simplicity-sys/vendor-simplicity.sh
+++ simplicity-sys/vendor-simplicity.sh
@@ -3,45 +3,70 @@ set -e
 
 # Requires (nix-shell with) git, rsync
 
+## 0. Parse command-line options
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "\$1 path to depend directory inside rust-simplicity-sys"
-  echo "\$2 path to libsimplicity repository root"
+  echo "Usage: $0 <path to depend directory> <path to libsimplicity repository root>"
   exit 1
 fi
 
-ORIGINAL_PATH=$(pwd)
-DEPEND_PATH=$1
-LIBSIM_PATH=$2
-VENDORED_SIM_DIR=simplicity
+DEPEND_PATH=$(readlink -f "$1")
+LIBSIM_PATH=$(readlink -f "$2")
+VENDORED_SIM_DIR="$DEPEND_PATH/simplicity"
 
-while true; do
-    read -r -p "$DEPEND_PATH/$VENDORED_SIM_DIR will be deleted [yn]: " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+## 1. Sanity check environment.
+if ! command -v git > /dev/null; then
+    echo "Missing 'git' executable in evironment."
+    exit 1
+fi
+if ! command -v rsync > /dev/null; then
+    echo "Missing 'rsync' executable in evironment."
+    exit 1
+fi
 
-rm -rf "$DEPEND_PATH/$VENDORED_SIM_DIR"
-cd "$LIBSIM_PATH"
+if [ ! -d "$DEPEND_PATH" ]; then
+    echo "Depend path '$DEPEND_PATH' does not appear to be a directory."
+    exit 1
+fi
+
+if [ ! -d "$LIBSIM_PATH" ]; then
+    echo "libsimplicity path '$LIBSIM_PATH' does not appear to be a directory."
+    exit 1
+fi
+
+if [ -d "$VENDORED_SIM_DIR" ]; then
+    while true; do
+        read -r -p "$VENDORED_SIM_DIR will be deleted [yn]: " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer y or n.";;
+        esac
+    done
+
+    rm -rf "$VENDORED_SIM_DIR"
+elif [ -e "$VENDORED_SIM_DIR" ]; then
+    echo "'simplicity' inside depend directory exists but appears not to be a directory."
+    echo "Please move or delete this file."
+    exit 1
+fi
+
+## 2. Copy files from libsimplicity
+pushd "$LIBSIM_PATH/C"
 
 if test -n "$(git status --porcelain)"; then
     echo "WARNING: libsimplicity repo is not clean"
 fi
 
 HEAD=$(git rev-parse HEAD)
-echo "# This file has been automatically generated." > "$ORIGINAL_PATH/$DEPEND_PATH/simplicity-HEAD-revision.txt"
-echo "$HEAD" >> "$ORIGINAL_PATH/$DEPEND_PATH/simplicity-HEAD-revision.txt"
+echo "# This file has been automatically generated." > "$DEPEND_PATH/simplicity-HEAD-revision.txt"
+echo "$HEAD" >> "$DEPEND_PATH/simplicity-HEAD-revision.txt"
 
 # Copy C folder to simplicity-sys/depend/simplicity
 # Use rsync to copy only files tracked by git
-cd C
-git ls-files | rsync -av --files-from=- . "$ORIGINAL_PATH/$DEPEND_PATH/$VENDORED_SIM_DIR"
-cd ..
+git ls-files | rsync -av --files-from=- . "$VENDORED_SIM_DIR"
 
-# We don't make any automatic source changes here. Optionally patch files
-# can be added here to automate the process. In future, we can have support for
-# symbols if required.
+popd
 
-cd "$ORIGINAL_PATH"
+## 3. Patch copied files.
+patch "$VENDORED_SIM_DIR/simplicity_alloc.h" "$DEPEND_PATH/simplicity_alloc.h.patch"
+
