985be76db1c64244dcf78b9f17d0b36a37a96994
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-07-14 20:22:03 +0000

    simplicity-sys: add versions to jets_ffi code
    The process here was:
    
    * Manually replace c_X with c_v0_3_X everywhere (actually only in two files,
      wrapper.h and jets_ffi.rs)
    * Run update_jets.sh and make sure it changes all the 0_3s to 0_4s.
    
    Note that now both update_jets.sh and vendor-simplicity.sh update wrapper.h.
    This is fine since the change is idempotent. But we really should clean up
    and combine these two scripts one of these days..
    
    The 0_3 test is easy to do during review, though if you are on NixOS then replace
    the cabal build/exec lines by first nix-building the `haskell` target in Simplicity
    and then directly calling result/bin/GenRustJets in there.

--- simplicity-sys/depend/wrapper.h
+++ simplicity-sys/depend/wrapper.h
@@ -2,7 +2,7 @@
 #define WRAPPER_H
 
 #define WRAP_(jet) \
-bool c_##jet(frameItem* dst, const frameItem* src, const txEnv* env) { \
+bool rustsimplicity_0_4_c_##jet(frameItem* dst, const frameItem* src, const txEnv* env) { \
   bool result = rustsimplicity_0_4_##jet(dst, *src, env); \
   rustsimplicity_0_4_assert(!result || 0 == dst->offset); \
   return result; \
