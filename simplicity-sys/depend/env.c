a9ee6067ef45fe6a7d23ddbf7c7d7aee4c17be97
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-07-14 20:41:46 +0000

    simplicity-sys: add versions to jet prefixes
    As before, I first manually added _0_3_ version numbers, modified the vendoring
    script, ran it, and made sure that the resulting diff added 0_4s and worked.

--- simplicity-sys/depend/env.c
+++ simplicity-sys/depend/env.c
@@ -3,32 +3,32 @@
 #include "simplicity/elements/env.h"
 #include "simplicity/primitive/elements/primitive.h"
 
-const size_t c_sizeof_rawBuffer = sizeof(rawBuffer);
-const size_t c_sizeof_rawOutput = sizeof(rawOutput);
-const size_t c_sizeof_rawInput = sizeof(rawInput);
-const size_t c_sizeof_rawTransaction = sizeof(rawTransaction);
-const size_t c_sizeof_rawTapEnv = sizeof(rawTapEnv);
-const size_t c_sizeof_txEnv = sizeof(txEnv);
+const size_t rustsimplicity_0_4_c_sizeof_rawBuffer = sizeof(rawBuffer);
+const size_t rustsimplicity_0_4_c_sizeof_rawOutput = sizeof(rawOutput);
+const size_t rustsimplicity_0_4_c_sizeof_rawInput = sizeof(rawInput);
+const size_t rustsimplicity_0_4_c_sizeof_rawTransaction = sizeof(rawTransaction);
+const size_t rustsimplicity_0_4_c_sizeof_rawTapEnv = sizeof(rawTapEnv);
+const size_t rustsimplicity_0_4_c_sizeof_txEnv = sizeof(txEnv);
 
-const size_t c_alignof_rawBuffer = alignof(rawBuffer);
-const size_t c_alignof_rawOutput = alignof(rawOutput);
-const size_t c_alignof_rawInput = alignof(rawInput);
-const size_t c_alignof_rawTransaction = alignof(rawTransaction);
-const size_t c_alignof_rawTapEnv = alignof(rawTapEnv);
-const size_t c_alignof_txEnv = alignof(txEnv);
+const size_t rustsimplicity_0_4_c_alignof_rawBuffer = alignof(rawBuffer);
+const size_t rustsimplicity_0_4_c_alignof_rawOutput = alignof(rawOutput);
+const size_t rustsimplicity_0_4_c_alignof_rawInput = alignof(rawInput);
+const size_t rustsimplicity_0_4_c_alignof_rawTransaction = alignof(rawTransaction);
+const size_t rustsimplicity_0_4_c_alignof_rawTapEnv = alignof(rawTapEnv);
+const size_t rustsimplicity_0_4_c_alignof_txEnv = alignof(txEnv);
 
-void c_set_rawBuffer(rawBuffer *result, const unsigned char *buf, unsigned int len)
+void rustsimplicity_0_4_c_set_rawBuffer(rawBuffer *result, const unsigned char *buf, unsigned int len)
 {
     *result = (rawBuffer){.buf = buf, .len = len};
 }
 
-void c_set_rawOutput(rawOutput *result, const unsigned char *asset, const unsigned char *value, const unsigned char *nonce, const rawBuffer *scriptPubKey,
+void rustsimplicity_0_4_c_set_rawOutput(rawOutput *result, const unsigned char *asset, const unsigned char *value, const unsigned char *nonce, const rawBuffer *scriptPubKey,
                      const rawBuffer *surjectionProof, const rawBuffer *rangeProof)
 {
     *result = (rawOutput){.asset = asset, .value = value, .nonce = nonce, .scriptPubKey = *scriptPubKey, .surjectionProof = *surjectionProof, .rangeProof = *rangeProof};
 }
 
-void c_set_rawInput(rawInput *result, const rawBuffer *annex, const unsigned char *pegin, const rawBuffer *scriptSig,
+void rustsimplicity_0_4_c_set_rawInput(rawInput *result, const rawBuffer *annex, const unsigned char *pegin, const rawBuffer *scriptSig,
                     const unsigned char *prevTxid, unsigned int prevIx,
                     const unsigned char *asset, const unsigned char *value, const rawBuffer *scriptPubKey,
                     unsigned int sequence,
@@ -38,7 +38,7 @@ void c_set_rawInput(rawInput *result, const rawBuffer *annex, const unsigned cha
     *result = (rawInput){.annex = annex, .scriptSig = *scriptSig, .prevTxid = prevTxid, .pegin = pegin, .issuance = {.blindingNonce = blindingNonce, .assetEntropy = assetEntropy, .amount = amount, .inflationKeys = inflationKeys, .amountRangePrf = *amountRangePrf, .inflationKeysRangePrf = *inflationKeysRangePrf}, .txo = {.asset = asset, .value = value, .scriptPubKey = *scriptPubKey}, .prevIx = prevIx, .sequence = sequence};
 }
 
-void c_set_rawTransaction(rawTransaction *result, unsigned int version,
+void rustsimplicity_0_4_c_set_rawTransaction(rawTransaction *result, unsigned int version,
                           const unsigned char *txid,
                           const rawInput *input, unsigned int numInputs,
                           const rawOutput *output, unsigned int numOutputs,
@@ -55,12 +55,12 @@ void c_set_rawTransaction(rawTransaction *result, unsigned int version,
     };
 }
 
-void c_set_rawTapEnv(rawTapEnv *result, const unsigned char *controlBlock, unsigned char pathLen, const unsigned char *scriptCMR)
+void rustsimplicity_0_4_c_set_rawTapEnv(rawTapEnv *result, const unsigned char *controlBlock, unsigned char pathLen, const unsigned char *scriptCMR)
 {
     *result = (rawTapEnv){.controlBlock = controlBlock, .pathLen = pathLen, .scriptCMR = scriptCMR};
 }
 
-void c_set_txEnv(txEnv *result, const transaction *tx, const tapEnv *taproot, const unsigned char *genesisHash, unsigned int ix)
+void rustsimplicity_0_4_c_set_txEnv(txEnv *result, const transaction *tx, const tapEnv *taproot, const unsigned char *genesisHash, unsigned int ix)
 {
     sha256_midstate genesis;
     sha256_toMidstate(genesis.s, genesisHash);
