be859b8bfc4a00f20d9c69b586ec051f05eb580b
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-07-14 20:22:03 +0000

    simplicity-sys: add versions to jets_ffi code
    The process here was:
    
    * Manually replace c_X with c_v0_3_X everywhere (actually only in two files,
      wrapper.h and jets_ffi.rs)
    * Run update_jets.sh and make sure it changes all the 0_3s to 0_4s.
    
    Note that now both update_jets.sh and vendor-simplicity.sh update wrapper.h.
    This is fine since the change is idempotent. But we really should clean up
    and combine these two scripts one of these days..
    
    The 0_3 test is easy to do during review, though if you are on NixOS then replace
    the cabal build/exec lines by first nix-building the `haskell` target in Simplicity
    and then directly calling result/bin/GenRustJets in there.

--- update_jets.sh
+++ update_jets.sh
@@ -29,9 +29,19 @@ cabal build -j8
 cabal exec GenRustJets
 
 cd "$RUST_DIR"
+
+DEFAULT_VERSION_CODE=$(grep "^version" "./simplicity-sys/Cargo.toml" | sed 's/\./_/g' | sed 's/.*"\(.*\)".*/\1/' | cut -d_ -f1-2)
+: "${SIMPLICITY_ALLOC_VERSION_CODE:=$DEFAULT_VERSION_CODE}"
+
 mv "${C_DIR}/jets_ffi.rs" "./simplicity-sys/src/c_jets/jets_ffi.rs"
 mv "${C_DIR}/jets_wrapper.rs" "./simplicity-sys/src/c_jets/jets_wrapper.rs"
 mv "${C_DIR}/jets_wrapper.c" "./simplicity-sys/depend/jets_wrapper.c"
+
+# Tweak the c_ prefixes in the wrappers
+sed -i -r "s/\"c_/\"rustsimplicity_${SIMPLICITY_ALLOC_VERSION_CODE}_c_/" \
+    "./simplicity-sys/src/c_jets/jets_ffi.rs"
+sed -i -r "s/ rustsimplicity_[0-9]+_[0-9]+_/ rustsimplicity_${SIMPLICITY_ALLOC_VERSION_CODE}_/" \
+    "./simplicity-sys/depend/wrapper.h"
 
 mv "${C_DIR}/core.rs" "./src/jet/init/"
 mv "${C_DIR}/bitcoin.rs" "./src/jet/init/"
