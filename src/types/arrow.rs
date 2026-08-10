3d8e48fdecbddbb18a8b28f29e5a782b11f58671
Andrew Poelstra <apoelstra@wpsoftware.net>
2026-08-10 20:37:12 +0000

    types: fix the use_self lint for the arrow module
    This was just kinda bugging me. It's better to use `Self` than
    explicitly writing `Arrow` both because it makes refactorings easier
    (e.g. if we copy/paste or macroize the code, it will require no changes
    to apply to a different type), and because `Self` is more specific than
    `Arrow`. That is, `Self` means `Arrow<'brand>` while just `Arrow` means
    `Arrow<'_>`. This improves compiler error messages when there are
    lifetime problems.

--- src/types/arrow.rs
+++ src/types/arrow.rs
@@ -67,7 +67,7 @@ impl fmt::Display for FinalArrow {
 impl FinalArrow {
     /// Same as [`Self::clone`] but named to make it clearer that this is cheap
     pub fn shallow_clone(&self) -> Self {
-        FinalArrow {
+        Self {
             source: Arc::clone(&self.source),
             target: Arc::clone(&self.target),
         }
@@ -85,7 +85,7 @@ impl<'brand> Arrow<'brand> {
 
     /// Same as [`Self::clone`] but named to make it clearer that this is cheap
     pub fn shallow_clone(&self) -> Self {
-        Arrow {
+        Self {
             source: self.source.shallow_clone(),
             target: self.target.shallow_clone(),
             inference_context: self.inference_context.shallow_clone(),
@@ -143,7 +143,7 @@ impl<'brand> Arrow<'brand> {
             )?;
         }
 
-        Ok(Arrow {
+        Ok(Self {
             source: prod_sum_a_b_c,
             target,
             inference_context: ctx,
@@ -177,7 +177,7 @@ impl<'brand> Arrow<'brand> {
 
         let prod_b_d = Type::product(ctx, b, d);
 
-        Ok(Arrow {
+        Ok(Self {
             source: a,
             target: prod_b_d,
             inference_context: lchild_arrow.inference_context.shallow_clone(),
@@ -191,7 +191,7 @@ impl<'brand> Arrow<'brand> {
         // the user during type inference, but in practice type inference
         // is completely opaque and there's no harm in making it moreso.
         let new = Type::free(inference_context, new_name("iden_src_"));
-        Arrow {
+        Self {
             source: new.shallow_clone(),
             target: new,
             inference_context: inference_context.shallow_clone(),
@@ -199,7 +199,7 @@ impl<'brand> Arrow<'brand> {
     }
 
     pub fn unit(inference_context: &Context<'brand>) -> Self {
-        Arrow {
+        Self {
             source: Type::free(inference_context, new_name("unit_src_")),
             target: Type::unit(inference_context),
             inference_context: inference_context.shallow_clone(),
@@ -207,7 +207,7 @@ impl<'brand> Arrow<'brand> {
     }
 
     pub fn injl(child: &Self) -> Self {
-        Arrow {
+        Self {
             source: child.source.shallow_clone(),
             target: Type::sum(
                 &child.inference_context,
@@ -219,7 +219,7 @@ impl<'brand> Arrow<'brand> {
     }
 
     pub fn injr(child: &Self) -> Self {
-        Arrow {
+        Self {
             source: child.source.shallow_clone(),
             target: Type::sum(
                 &child.inference_context,
@@ -231,7 +231,7 @@ impl<'brand> Arrow<'brand> {
     }
 
     pub fn take(child: &Self) -> Self {
-        Arrow {
+        Self {
             source: Type::product(
                 &child.inference_context,
                 child.source.shallow_clone(),
@@ -243,7 +243,7 @@ impl<'brand> Arrow<'brand> {
     }
 
     pub fn drop_(child: &Self) -> Self {
-        Arrow {
+        Self {
             source: Type::product(
                 &child.inference_context,
                 Type::free(&child.inference_context, new_name("drop_src_")),
@@ -261,7 +261,7 @@ impl<'brand> Arrow<'brand> {
             &right.source,
             "comp combinator: left target = right source",
         )?;
-        Ok(Arrow {
+        Ok(Self {
             source: left.source.shallow_clone(),
             target: right.target.shallow_clone(),
             inference_context: left.inference_context.shallow_clone(),
@@ -287,7 +287,7 @@ impl<'brand> Arrow<'brand> {
             &right.source,
             "pair combinator: left source = right source",
         )?;
-        Ok(Arrow {
+        Ok(Self {
             source: left.source.shallow_clone(),
             target: Type::product(
                 &left.inference_context,
@@ -299,7 +299,7 @@ impl<'brand> Arrow<'brand> {
     }
 
     pub fn fail(inference_context: &Context<'brand>) -> Self {
-        Arrow {
+        Self {
             source: Type::free(inference_context, new_name("fail_src_")),
             target: Type::free(inference_context, new_name("fail_tgt_")),
             inference_context: inference_context.shallow_clone(),
@@ -307,7 +307,7 @@ impl<'brand> Arrow<'brand> {
     }
 
     pub fn const_word(inference_context: &Context<'brand>, word: &Word) -> Self {
-        Arrow {
+        Self {
             source: Type::unit(inference_context),
             target: Type::two_two_n(inference_context, word.n()),
             inference_context: inference_context.shallow_clone(),
@@ -317,7 +317,7 @@ impl<'brand> Arrow<'brand> {
     pub fn jet(inference_context: &Context<'brand>, jet: &dyn Jet) -> Self {
         inference_context.check_jet(jet);
 
-        Arrow {
+        Self {
             source: jet.source_ty().to_type(inference_context),
             target: jet.target_ty().to_type(inference_context),
             inference_context: inference_context.shallow_clone(),
@@ -325,7 +325,7 @@ impl<'brand> Arrow<'brand> {
     }
 
     pub fn witness(inference_context: &Context<'brand>) -> Self {
-        Arrow {
+        Self {
             source: Type::free(inference_context, new_name("witness_src_")),
             target: Type::free(inference_context, new_name("witness_tgt_")),
             inference_context: inference_context.shallow_clone(),
@@ -337,7 +337,7 @@ impl<'brand> Arrow<'brand> {
     }
 }
 
-impl<'brand> DisconnectConstructible<'brand, Arrow<'brand>> for Arrow<'brand> {
+impl<'brand> DisconnectConstructible<'brand, Self> for Arrow<'brand> {
     fn disconnect(left: &Self, right: &Self) -> Result<Self, Error> {
         Self::for_disconnect(left, right)
     }
@@ -349,7 +349,7 @@ impl<'brand> DisconnectConstructible<'brand, NoDisconnect> for Arrow<'brand> {
         let target = Type::free(&left.inference_context, "disc_tgt".into());
         Self::for_disconnect(
             left,
-            &Arrow {
+            &Self {
                 source,
                 target,
                 inference_context: left.inference_context.shallow_clone(),
@@ -358,7 +358,7 @@ impl<'brand> DisconnectConstructible<'brand, NoDisconnect> for Arrow<'brand> {
     }
 }
 
-impl<'brand> DisconnectConstructible<'brand, Option<&Arrow<'brand>>> for Arrow<'brand> {
+impl<'brand> DisconnectConstructible<'brand, Option<&Self>> for Arrow<'brand> {
     fn disconnect(left: &Self, right: &Option<&Self>) -> Result<Self, Error> {
         match *right {
             Some(right) => Self::disconnect(left, right),
